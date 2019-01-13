ec811032f2d26072150a6ff0f5676270ca9b4340
Deepank Chauhan <31995183+deepankchauhan@users.noreply.github.com>
2019-01-14 00:57:25 +0530

    Creating a right aligned pyramid

---
+++ mario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int height, rows;
+    // this prompts the user to give an input
+    do
+    {
+        printf("Height: ");
+        scanf("%d",&height);
+    }
+    while (height < 1 || height > 23);
+
+    // logic
+    for (rows = 0; rows < height; rows++)
+    {
+        // this adds space before the '#'
+        for (int space = (height - rows) - 1; space > 0; space-- )
+        {
+            printf(" ");
+        }
+        // this adds the '#'
+        for (int hash = 0; hash <= rows + 1; hash++)
+        {
+            printf("#");
+        }
+        // moves it to the next line
+        printf("\n");
+    }
+
+    return 0;
+}
