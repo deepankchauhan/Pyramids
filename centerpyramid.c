4f007a42d9c6a1fd9e68e7779a474100f92ab57d
Deepank Chauhan <31995183+deepankchauhan@users.noreply.github.com>
2019-01-15 00:30:54 +0530

    Create a center aligned pyramid

--- centerpyramid.c
+++ centerpyramid.c
@@ -1,37 +1,45 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
     int height, rows;
-    // this prompts the user to give an input
+
+    // this prompts user to give an input
     do
     {
-
         do
         {
-            printf("height: ");
+            printf("height:");
             while(getchar() != '\n');
         }
-        while(scanf("%d",&height) != 1);
+        while(scanf("%d", &height) != 1);
     }
     while (height < 1 || height > 23);
 
     // logic
     for (rows = 0; rows < height; rows++)
     {
-        // this adds space before the '#'
-        for (int space = (height - rows) - 1; space > 0; space-- )
+        // adds space before '#'
+        for (int space = (height - rows) - 1; space > 0; space--)
         {
             printf(" ");
         }
-        // this adds the '#'
-        for (int hash = 0; hash <= rows + 1; hash++)
+
+        // the loop for iterating the '#'
+        for (int hash = 0; hash <= rows; hash++)
         {
             printf("#");
+
         }
-        // moves it to the next line
+        printf("  ");
+        for (int hash = 0; hash <= rows; hash++)
+        {
+            printf("#");
+
+        }
         printf("\n");
+
     }
 
-    return 0;
 }
